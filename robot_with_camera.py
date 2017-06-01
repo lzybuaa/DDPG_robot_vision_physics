23b6086729efb57eaccefc9b640c19a6f83a8c2b
Siwei Guo <sisama2323@gmail.com>
2017-05-31 20:46:36 -0700

    add urfd file

--- robot_with_camera.py
+++ robot_with_camera.py
@@ -7,23 +7,29 @@ import os
 path = os.getcwd()
 p.connect(p.GUI)
 p.resetSimulation()
-p.setGravity(0,0,-9.8)
-kukaId = p.loadURDF(path + "\kuka.urdf",[0,0,0],useFixedBase=True)
-kukaJoint=p.getNumJoints(kukaId)
-ballId = p.loadURDF(path + "\sphere_small.urdf",[0.3,-0.3,2],useFixedBase=True)
-p.changeVisualShape(ballId,0,rgbaColor=(0,1,0,1))  # green ball
+p.setGravity(0, 0, -9.8)
+kukaId = p.loadURDF(path + "\\kuka_lwr\kuka.urdf", (0, 0, 0), useFixedBase=True)
+numjoint = p.getNumJoints(kukaId)
+ballId = p.loadURDF(path + "\\ball\sphere_small.urdf", (1, -1, 0.7), useFixedBase=True)
+p.changeVisualShape(ballId, 0, rgbaColor=(0, 1, 0, 1))  # green ball
+#p.changeDynamics(ballId, 0, mass=1.0)
 
-for i in range(1000):   # make ball free fall
-    p.stepSimulation()
-    p.setRealTimeSimulation(1)
-    p.applyExternalForce(ballId,-1,(0,0,-9.8),(0,0,0),flags=2)
-    ballPos,ballOrn=p.getBasePositionAndOrientation(ballId)
+ballpos,ballorn = p.getBasePositionAndOrientation(ballId)
+# inverse kinematic
+jointinit = p.calculateInverseKinematics(kukaId,numjoint-1,(0,0.2,0.7))
+# set initial pos for robot
+for i in range (numjoint):
+	p.resetJointState(kukaId,i,jointinit[i])
+sleep(0.01)
 
-endPos,endOrn=p.getLinkState(kukaId,6)[0:2]   # position and orientation of end effector
+# position and orientation of end effector
+endPos,endOrn=p.getLinkState(kukaId,6)[0:2]   
 rotmatrix=p.getMatrixFromQuaternion(endOrn)
-distance=0.1   # distance from camera to focus
+ # distance from camera to focus
+distance=0.1
 
-camview=list()   # where does camera aim at
+# where does camera aim at
+camview=list()   
 for i in range(3):
     camview.append(np.dot(rotmatrix[i*3:i*3+3],(0,0,-distance)))
 camview[2]=-camview[2]
@@ -34,9 +40,15 @@ tagPos=np.add(camview,endPos)
 
 viewMatrix=p.computeViewMatrix(endPos,tagPos,(0,1,0))
 projectMatrix = p.computeProjectionMatrixFOV(60,1,0.1,100)     # input: field of view, ratio(width/height),near,far
-rgbpix,depthpix=p.getCameraImage(512,512,viewMatrix,projectMatrix)[2:4]   # input: image resolution
+rgbpix,depthpix=p.getCameraImage(128,128,viewMatrix,projectMatrix)[2:4]   # input: image resolution
+
+# free fall ball
+for i in range(1000):   
+    p.stepSimulation()
+    p.setRealTimeSimulation(1)
+    p.applyExternalForce(ballId,-1,(0,0,-9.8),(0,0,0),flags=2)
+    ballPos,ballOrn=p.getBasePositionAndOrientation(ballId)
 
 for i in range (1,10):
     p.stepSimulation()
-    sleep(1)
-
+    sleep(0.01)
