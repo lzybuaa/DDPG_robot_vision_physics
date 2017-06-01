694cc68b10f3b14817e4d64f2e96b368a546242a
Siwei Guo <sisama2323@gmail.com>
2017-06-01 15:15:04 -0700

    initial position fixed to aim at the ball

--- robot_with_camera.py
+++ robot_with_camera.py
@@ -13,8 +13,6 @@ p.setGravity(0, 0, -9.8)
 kukaId = p.loadURDF(path + "\\kuka_lwr\kuka.urdf", (0, 0, 0), useFixedBase=True)
 groundId = p.loadURDF(path + "\\floor\plane100.urdf", (0, 0, 0), useFixedBase=True)
 numjoint = p.getNumJoints(kukaId)
-ballId = p.loadURDF(path + "\\ball\sphere_small.urdf", (1.5, 0, 0.9), useFixedBase=True)  # free fall after robot setup the initial position
-
 #plate = p.loadURDF("D:/bullet3-master/data/dinnerware/plate.urdf", (1, 1, 0.5), useFixedBase=True)
 #pan = p.loadURDF("D:/bullet3-master/data/dinnerware/pan_tefal.urdf", (-1, 1, 0.5), useFixedBase=True)
 #duck = p.loadURDF("D:/bullet3-master/data/duck_vhacd.urdf", (-1, -1, 0.5), useFixedBase=True)
@@ -27,7 +25,6 @@ ballId = p.loadURDF(path + "\\ball\sphere_small.urdf", (1.5, 0, 0.9), useFixedBa
 for i in range(1,20):
     p.stepSimulation()
 
-ballpos, ballorn = p.getBasePositionAndOrientation(ballId)
 initpos = (0.5, 0, 0.9)
 initorn = ([0, math.pi/2, 0])
 # set initial pos for robot
@@ -55,16 +52,19 @@ tagPos=np.add(camview,endPos)
 #endOrn=np.divide(p.getEulerFromQuaternion(endOrn),(2*math.pi))*360
 #viewMatrix = p.computeViewMatrixFromYawPitchRoll(endPos,distance,-endOrn[2],-endOrn[1],endOrn[0]+180,2)   # camera doesn't change its view angle, robot does
 
+# after the robot reach to its initial position put the ball inside
+ballId = p.loadURDF(path + "\\ball\sphere_small.urdf", (1.5, 0, 0.9), useFixedBase=False)  # free fall after robot setup the initial position
+ballpos, ballorn = p.getBasePositionAndOrientation(ballId)
+
 #p.removeBody(kukaId)
 viewMatrix=p.computeViewMatrix(endPos,tagPos,(0,0,1))
 #viewMatrix=p.computeViewMatrix([-2, 0, 2], [0, 0, 1],[0, 0, 1])
 projectMatrix = p.computeProjectionMatrixFOV(60,1,0.1,100)     # input: field of view, ratio(width/height),near,far
 rgbpix=p.getCameraImage(400,400,viewMatrix,projectMatrix)   # input: image resolution
 
-
 imgplot = plt.imshow(np.reshape(np.array(rgbpix[2])/255.0, (400, 400, 4)))
 plt.show()
 
-for i in range (1,10):
+for i in range (2000):
     p.stepSimulation()
-    sleep(0.01)
+
